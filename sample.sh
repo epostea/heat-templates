e3ebf4e09742558c940821944798e1384f7dd777
Eugen Postea <epostea@gmail.com>
2025-02-24 11:50:36 -0500

    test1

--- sample.sh
+++ sample.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-kubectl describe pod <pod_name>
+# Define the deployment name and desired number of replicas
+deployment_name="my-deployment"
+desired_replicas=5
+
+# Scale the deployment to match the desired number of replicas
+kubectl scale deployment $deployment_name --replicas=$desired_replicas
