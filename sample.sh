c8875ec7d693a3a3fa3e31374a6cb3334a68524b
Eugen Postea <epostea@gmail.com>
2025-02-24 12:11:04 -0500

    test

--- sample.sh
+++ sample.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-
-# Define the deployment name and desired number of replicas
-deployment_name="my-deployment"
-desired_replicas=5
-
-# Scale the deployment to match the desired number of replicas
-kubectl scale deployment $deployment_name --replicas=$desired_replicas
+# Check for connections that failed to start from this network device
+netstat -ntpl 2> /dev/null | grep :80 | grep -v 0.0.0.0 | awk '{print $5}' | cut -d: -f1 | sort | uniq -c | sort -nr | less
